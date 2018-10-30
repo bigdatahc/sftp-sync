d98293850b4dca9b0ac62488aa595fb023ec1b5e
Scot Kronenfeld <skronen@searshc.com>
2018-10-30 12:03:54 -0500

    Some small fixes

--- sftp-sync.py
+++ sftp-sync.py
@@ -13,7 +13,7 @@ def parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("config")
-    parser.add_argument("--dry-run", "dryrun",
+    parser.add_argument("--dry-run", action='store_true', default=False,
                         help="Print what would happen, but don't transfer anything")
 
     return parser.parse_args()
@@ -34,11 +34,15 @@ class SftpSync:
 
     default_port = 22
     
-    def __init__(self, source, dest, dry_run=False, hooks=None):
-        self.source = self.get_sftp_connection(source)
-        self.dest = self.get_sftp_connection(dest)
+    def __init__(self, config, dry_run=False):
+        self.source = self.get_sftp_connection(config['source'])
+        self.dest = self.get_sftp_connection(config['dest'])
+
+        self.hooks = None
+        if config.has_section('hooks'):
+            self.hooks = config['hooks']
+            
         self.dry_run = dry_run
-        self.hooks = hooks
         self.file_details = {}
 
     def _validate_config(self, config):
@@ -47,7 +51,8 @@ class SftpSync:
                 print("ERROR: Missing key `{}`".format(key))
                 sys.exit(1)
 
-        config['PORT'] = self._validate_port(config)
+        # configparser requires storing as a string
+        config['PORT'] = str(self._validate_port(config))
         return config
 
     def _validate_port(self, config):
@@ -64,7 +69,7 @@ class SftpSync:
     def get_sftp_connection(self, config):
         self._validate_config(config)
 
-        transport = paramiko.Transport((config['HOST'], config['PORT']))
+        transport = paramiko.Transport((config['HOST'], int(config['PORT'])))
         transport.connect(
             username=config['USER'],
             password=config['PASS']
@@ -83,7 +88,7 @@ class SftpSync:
         diff = set(source_files) - set(dest_files)
         for filename in diff:
             if self.dry_run:
-                print("Would transfer {} (but --dry-run enabled, nothing will be transferred").format(filename)
+                print("Would transfer {}".format(filename))
             else:
                 self.transfer_file(filename)
 
@@ -115,8 +120,11 @@ class SftpSync:
 
 def main():
     args = parse_args()
+    if args.dry_run:
+        print("--dry-run specified.  Nothing will be transferred\n")
+
     config = get_config(args.config)
-    sftp_sync = SftpSync(config['source'], config['dest'], config.get['hooks'])
+    sftp_sync = SftpSync(config, args.dry_run)
     sftp_sync.sync()
     
 
